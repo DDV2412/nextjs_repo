14cd2d6fd1cda740cc83886760e2f1872f5031cd
DDV2412 <dhyanputra24@gmail.com>
2023-10-06 10:40:17 +0700

    Update Domain

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['127.0.0.1'],
-      },
-}
+  images: {
+    domains: ['127.0.0.1', '103.102.152.252'],
+  },
+};
 
 module.exports = nextConfig
